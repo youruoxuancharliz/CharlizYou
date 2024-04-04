643a59470d9a94f201a36151654edaef1112a412
youruoxuancharliz <161728549+youruoxuancharliz@users.noreply.github.com>
2024-04-05 00:40:58 +0800

    Update SPS.py

--- SPS.py
+++ SPS.py
@@ -8,21 +8,22 @@ print( "Welcome to the game," + x + "!")
 player= input("Rock, Paper or Scissors?")
 
  #Remember to add indents when needed. Tabs are preferred.
-if player.lower == computer
+#Remember to add " : "
+if player.lower == computer :
   print( "Its a tie! Try again." )
-elif player.lower == "rock" and computer == "scissors" 
+elif player.lower == "rock" and computer == "scissors" :
  print(computer)
  print("You win!")
  
-elif player.lower == "paper" and computer == "rock"
+elif player.lower == "paper" and computer == "rock" :
  print(computer)
  print("You win!")
 
-elif player.lower == "scissors" and computer == "paper"
+elif player.lower == "scissors" and computer == "paper" :
  print(computer)
  print("You win!")
 
-else print("Scissors, Paper, Stone?")
+else : print("Scissors, Paper, Stone?")
       
 
 
