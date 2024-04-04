f1930d23a9ff979e1b4213bd8d9f9e2339853d9a
youruoxuancharliz <161728549+youruoxuancharliz@users.noreply.github.com>
2024-04-05 00:37:32 +0800

    Scissors Paper Stone GAME.py

--- SPS.py
+++ SPS.py
@@ -1,25 +1,26 @@
 import random
- options = ( "rock", "paper", "scissors")
-  print( "The Game of Scissors Paper Stone")
+options = ( "rock", "paper", "scissors")
+print( "The Game of Scissors Paper Stone")
 computer = options[random.randint(0,2)]
-  print( "Hi, Introduce yourself!" )
- x = input()
-  print( "Welcome to the game!" + x )
+print( "Hi, Introduce yourself!" )
+x = input()
+print( "Welcome to the game!" + x )
 player= input("Rock, Paper or Scissors?")
 
-  if player.lower == computer
-print( "Its a tie! Try again." )
- elif player.lower == "rock" and computer == "scissors" 
-print(computer)
-print("You win!")
-
+ #Remember to add indents when needed. Tabs are preferred.
+if player.lower == computer
+  print( "Its a tie! Try again." )
+elif player.lower == "rock" and computer == "scissors" 
+ print(computer)
+ print("You win!")
+ 
 elif player.lower == "paper" and computer == "rock"
-print(computer)
-print("You win!")
+ print(computer)
+ print("You win!")
 
 elif player.lower == "scissors" and computer == "paper"
-print(computer)
-print("You win!")
+ print(computer)
+ print("You win!")
 
 else print("Scissors, Paper, Stone?")
       
