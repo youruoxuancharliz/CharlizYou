fefcd120689d7edfbbba840e07a2d7d88a796a1f
youruoxuancharliz <161728549+youruoxuancharliz@users.noreply.github.com>
2024-04-05 01:16:45 +0800

    SPS.py

--- SPS.py
+++ SPS.py
@@ -5,25 +5,26 @@ computer = options[random.randint(0,2)]
 print( "Hi, Introduce yourself! Whats your name?" )
 x = input()
 print( "Welcome to the game," + x + "!")
-player= input("Rock, Paper or Scissors?")
+player= input("Choose Rock, Paper or Scissors?")
 
  #Remember to add indents when needed. Tabs are preferred.
 #Remember to add " : "
-if player.lower == computer :
+while True :
+ if player.lower == computer :
   print( "Its a tie! Try again." )
-elif player.lower == "rock" and computer == "scissors" :
- print(computer)
- print("You win!")
+ elif player.lower == "rock" and computer == "scissors" :
+  print(computer)
+  print("You win!")
  
-elif player.lower == "paper" and computer == "rock" :
- print(computer)
- print("You win!")
+ elif player.lower == "paper" and computer == "rock" :
+  print(computer)
+  print("You win!")
 
-elif player.lower == "scissors" and computer == "paper" :
- print(computer)
- print("You win!")
+ elif player.lower == "scissors" and computer == "paper" :
+  print(computer)
+  print("You win!")
 
-else : print("Scissors, Paper, Stone?")
+ else : print("Scissors, Paper, Stone?")
       
 
 
