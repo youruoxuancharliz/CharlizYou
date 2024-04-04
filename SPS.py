6a524025ec4cee4c9384646a8a4eb0cb82dded86
youruoxuancharliz <161728549+youruoxuancharliz@users.noreply.github.com>
2024-04-05 00:31:57 +0800

    scissors paper stome GAME.py

--- SPS.py
+++ SPS.py
@@ -1,49 +1,30 @@
 import random
+ options = ( "rock", "paper", "scissors")
+  print( "The Game of Scissors Paper Stone")
+computer = options[random.randint(0,2)]
+  print( "Hi, Introduce yourself!" )
+ x = input()
+  print( "Welcome to the game!" + x )
+player= input("Rock, Paper or Scissors?")
+
+  if player.lower == computer
+print( "Its a tie! Try again." )
+ elif player.lower == "rock" and computer == "scissors" 
+print(computer)
+print("You win!")
+
+elif player.lower == "paper" and computer == "rock"
+print(computer)
+print("You win!")
+
+elif player.lower == "scissors" and computer == "paper"
+print(computer)
+print("You win!")
+
+else print("Scissors, Paper, Stone?")
+      
 
 
 
-
-while True:
-    try:
-        print('enter 1 for stone, 2 for paper, 3 for scissors and 0 for quit')
-        player = int(input()) 
-    except ValueError:
-        print('Oops!!! letters not allowed...\n\n')
-        continue
-
-     
-   
-    computer = random.randint(1,3)
-     
-    if player == 0:
-        break    
-        
-    elif player == computer:
-        print(computer)
-        print('match tie...\n')
-    elif player == 1 and computer == 2:
-        print(computer)
-        print('computer wins!!\n')
-    elif player == 1 and computer == 3:
-        print(computer)
-        print('player wins!!\n')
-    elif player == 2 and computer == 1:
-        print(computer)
-        print('player wins!!\n')
-        
-    elif player == 2 and computer == 3:
-        print(computer)
-        print('computer wins.. \n')
-        
-    elif player == 3 and computer == 1:
-        print(computer)
-        print('computer wins..\n')
-    elif player == 3 and computer == 2:
-        print(computer)
-        print('player wins!!\n')
-    else:
-        print('\n enter the correct number.....\n \n  ')
-        continue
-        
         
     
