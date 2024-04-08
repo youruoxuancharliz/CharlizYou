13ef6fb0ef4e60726426076c3d36784e86d0fb9d
youruoxuancharliz <161728549+youruoxuancharliz@users.noreply.github.com>
2024-04-09 01:51:57 +0800

    Python Decision Making.py

--- Python Decision Making.py
+++ Python Decision Making.py
@@ -1,14 +1,16 @@
 import random
-allmovies = ["Top Gun Maverick", "The Social Network", "Spider-Man", "Glass Onion", "Files of The Unexplained"]
+allmovies = ["Top Gun Maverick", "The Social Network", "Spider-Man", "Glass Onion", "Enola Holmes"]
 allseries = ["Brooklyn 99", "Suits", "Young Sheldon", "Superstore"]
 
-
+print("Making your decisions.....")
 me = input("movie or series?")
 #include quotation marks 
 
-if me == "movie" or "movies" :
-  print( allmovies[random.randint(0,4)])
-elif me == "series" :  
-     print( allseries[random.randint(0,3)])
-
-else : print("choose between movie or series!")
+if me == "series":
+    print( allseries[random.randint(0,3)])
+    print( "This series is chosen.")
+elif me == "movie" or "movies" :
+     print( allmovies[random.randint(0,4)])
+     print( "This movie is chosen.")
+else : 
+      print("choose an option.")
