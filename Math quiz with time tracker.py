fe0ed27793ec8bf8c98d7967bbb9a0f873c4106d
youruoxuancharliz <161728549+youruoxuancharliz@users.noreply.github.com>
2024-04-09 04:00:22 +0800

    Math quiz with time tracker.py

--- Math quiz with time tracker.py
+++ Math quiz with time tracker.py
@@ -3,10 +3,14 @@ import time
 num1 = random.randint(0,10)
 num2 = random.randint(0,10)
 
-# DEFINE ALL THE QUESTIONS AND ANSWERS first.
+hardnum1 = random.randint(20,100)
+hardnum2 = random.randint(20,100)
+hardadditionqn = str(hardnum1) + ' + ' + str(hardnum2) + ' =' + ' ?'
 
+
+# DEFINE ALL THE QUESTIONS AND ANSWERS
 additionqn = str(num1) + ' + ' + str(num2) + ' =' + ' ?'
-additionanswer = num1 + num2
+
 
 multiplicationqn = str(num1)+ ' * '+ str(num2)+ ' =' + ' ?'
 multiplicationans = num1*num2
@@ -19,12 +23,18 @@ minusans = num1-num2
 
 
 print('====MATH TIMED QUIZ====')
+# ======== SETTING DIFFICULTY LEVEL ===========
+difficulty = input('Easy level with all operators or attempt difficult addition questions?')
+if difficulty == 'difficult' :
+ print(hardadditionqn)
+ ans = input('Enter your answer :')
+ if ans == str(hardnum1 + hardnum2) :
+     print('You got it right!')
+ else : print("Wrong answer... Think again!")
 
-
-
-chosen_operator = input('choose an operator : -, +, *, /')
-
-if chosen_operator == '+' :
+elif difficulty == 'easy' or 'easy level' :
+ chosen_operator = input('choose an operator : -, +, *, /')
+ if chosen_operator == '+' :
     print('Here is your question')
     start_time = time.time()
     print (additionqn)
@@ -32,13 +42,13 @@ if chosen_operator == '+' :
     playeranswer = int(input('Enter your answer'))
     end_time = time.time()
     totaltime = end_time - start_time
-    if playeranswer == additionanswer :
+    if playeranswer == num1 + num2 :
          print("Good Job. You got it right!")
          print('You did it in...')
          print (totaltime, 'seconds!' )
     else : print("Wrong answer.. Think again!")
 
-elif  chosen_operator == '*' :
+ elif  chosen_operator == '*' :
     print(multiplicationqn)
     start_time = time.time()
     playeranswer = int(input('Enter your answer'))
@@ -48,7 +58,7 @@ elif  chosen_operator == '*' :
      print('You got it right! In..', totaltime, 'seconds!')
     else : print("Wrong answer.. Think again!")
           
-elif chosen_operator == '/'  :
+ elif chosen_operator == '/'  :
       print('If answer is a decimal, then Answer to the according integer.')
       print(divisionqn)
       start_time = time.time()
@@ -59,7 +69,7 @@ elif chosen_operator == '/'  :
           print('You got it right! In..', totaltime, 'seconds!')
       else : print("Wrong answer.. Think again!")
       
-elif chosen_operator == '-'  :
+ elif chosen_operator == '-'  :
       print(minusqn)
       start_time = time.time()
       playeranswer = int(input('Enter your answer'))
@@ -69,3 +79,4 @@ elif chosen_operator == '-'  :
           print('You got it right! In..', totaltime, 'seconds!')
       else : print("Wrong answer.. Think again!")
     
+
