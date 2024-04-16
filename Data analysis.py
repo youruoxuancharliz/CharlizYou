6033086a2bcaa070e435ee798cb85225b3070323
youruoxuancharliz <161728549+youruoxuancharliz@users.noreply.github.com>
2024-04-16 16:56:44 +0800

    Update Data analysis.py

--- Data analysis.py
+++ Data analysis.py
@@ -4,4 +4,13 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 data=pd.read_csv("nba.csv")
+# Drop data table down
 data
+
+# Print info of data
+print( data.info() )
+
+# Return top few and bottom rows
+print(data.head(6))
+print(data.tail(10))
+
