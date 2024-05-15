822b5972802403e6a32cf7a9f53ebaa2c17d32ed
youruoxuancharliz <161728549+youruoxuancharliz@users.noreply.github.com>
2024-05-15 17:14:17 +0800

    Update pandas data  .py

--- pandas data  .py
+++ pandas data  .py
@@ -20,6 +20,9 @@ data.groupby('Name','Salary')
 # return specific columns and rows
 data.loc[0,['Name','Salary']]   # row index 0
 
+# sorting values of named columns in order, eg descending
+data.sort_values("Salary", ascending=False)
+
 # delete column eg college
 data.pop('College') 
 
